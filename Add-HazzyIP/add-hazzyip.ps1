36da26848996a71146ae2081d77ca266f78f1f45
wevans311082 <wayne.edward.evans@googlemail.com>
2016-12-21 16:34:31 +0000

    Add Files

---
+++ Add-HazzyIP/add-hazzyip.ps1
@@ -0,0 +1,64 @@
+﻿#requires -version 2
+<#
+.SYNOPSIS
+  Add/Update Hazzy's dynamic DNS IP into a Windows Firewall Rule for SMB Access
+.DESCRIPTION
+  This script resolves the IP of a DNS record for home.hazzy.co.uk and then uses that in the creation or updating if the rule already exists it updates the IP
+.PARAMETER 
+    
+.INPUTS
+  
+.OUTPUTS
+  
+.NOTES
+  Version:        1.0
+  Author:         Grumpy Admin
+  Creation Date:  08/12/2016
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  add-hazzyipwf
+#>
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+function resolve-hazzyip{
+$ip= $(Resolve-DnsName -Name home.hazzy.co.uk).IP4Address
+return $ip
+}
+function update-wf{
+  Param(
+  [parameter(Mandatory=$true)]
+  [String] $ip
+  )
+  
+  Begin{
+   
+  }
+  
+  Process{
+     
+         try {
+                Get-NetFirewallRule -Name "Hazzy SMB Access inbound port 445" -ErrorAction stop | Set-NetFirewallRule -RemoteAddress $ip,"127.0.0.1"         
+             }
+         catch {         
+                New-NetFirewallRule -Name "Hazzy SMB Access inbound port 445" -DisplayName "Hazzy SMB Access inbound port 445" -Action "Allow" -Profile "Domain,Public,Private" -Direction "InBound"  -RemoteAddress $ip -RemotePort "445" -LocalPort "445" -Protocol "TCP"
+        }
+  }
+  
+  End{  
+  }
+}
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+update-wf -ip $(resolve-hazzyip)
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # grumpyadmin
 Grumpy Admin Powershell Scripts
+
+This is a working area - a muddle of crap - mainly cloned for backups
